edff886a7e6a5a34099b137baf10fbd2329f0eda
Sidedig <sidedig01@gmail.com>
2024-04-10 00:08:24 -0600

    Merge branch 'main' of https://github.com/SideDig/ionic-appss

--- api1/index.php
+++ api1/index.php
@@ -11,27 +11,21 @@ class Router {
     }
 
     public function route($url) {
-        // Separar la URL por "/" y tomar el primer segmento
-        $urlParts = explode('/', $url);
-        $route = '/' . $urlParts[1]; // Tomar el primer segmento como ruta
-
-        if (isset($this->routes[$route])) {
-            require $this->routes[$route];
+        if (isset($this->routes[$url])) {
+            require $this->routes[$url];
         } else {
-            http_response_code(404); // Establece el código de respuesta HTTP adecuado
             echo "404 Not Found";
         }
     }
 }
 
 $router = new Router();
-// Tus rutas existentes
 $router->addRoute('/usuarios', './routes/usuarios.php');
 $router->addRoute('/productos', './routes/productos.php');
-$router->addRoute('/banners', './routes/banners.php');
++$router->addRoute('/banners', './routes/banners.php');
 $router->addRoute('/auth', './routes/auth.php');
 
-// Modificar la línea que procesa la URL para eliminar cualquier parámetro de consulta
-$url = strtok(str_replace('/api1', '', $_SERVER['REQUEST_URI']), '?');
+
+$url = str_replace('/api1', '', $_SERVER['REQUEST_URI']);
 $router->route($url);
 ?>

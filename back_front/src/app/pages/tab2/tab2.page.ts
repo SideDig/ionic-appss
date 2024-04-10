37c1a77cfd49a7466b61912960cb81adb948eb23
Sidedig <sidedig01@gmail.com>
2024-04-10 00:07:46 -0600

    Revert "Merge branch 'main' of https://github.com/SideDig/ionic-appss"
    This reverts commit b9f06bf5cee546ef6c94cabc7a269543b47d3018.

--- back_front/src/app/pages/tab2/tab2.page.ts
+++ back_front/src/app/pages/tab2/tab2.page.ts
@@ -1,37 +1,19 @@
 import { Component } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
-import { productos } from 'src/app/interfaces'; // Importar la clase productos desde index.ts
-import { Router } from '@angular/router';
-
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service'; 
+import { productos } from 'src/app/interfaces';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  id_producto?: number;
-  nombre_producto?: string;
-  descripcion_producto?: string;
-  precio_producto?: number;
-  imagen_producto?: string;
-  nombre_categoria?: number;
+  productos$: Observable<productos[]>; 
+  terminoBusqueda = ''; 
 
-  constructor(private apiService: ApiService, private router: Router) {}
+  constructor(private apiService: ApiService) {}
 
-  enviarDatos() {
-    const datos: productos = {
-      id_producto: this.id_producto, 
-      nombre_producto: this.nombre_producto,
-      descripcion_producto: this.descripcion_producto,
-      precio_producto: this.precio_producto,
-      imagen_producto: this.imagen_producto,
-      nombre_categoria: this.nombre_categoria
-    };
-
-    this.apiService.postDatos(datos).subscribe(resp => {
-      console.log(resp);
-      this.router.navigate(['/tabs']);
-
-    });
+  buscar() {
+    this.productos$ = this.apiService.buscarProductos(this.terminoBusqueda);
   }
 }
